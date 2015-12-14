aca0772754bcbf8c7e97f8be06b18298962971ab
Grant Timmerman <granttimmerman@gmail.com>
2015-12-14 13:17:33 -0800

    FINAL WITHOUT COLLISIONS

--- public/js/agentFactory.js
+++ public/js/agentFactory.js
@@ -219,8 +219,8 @@ var carFactory = function(carCenter, isClient) {
 
   var carComposite = Composite.create();
   Composite.addBody(carComposite, carBody);
-  // Composite.addBody(carComposite, carShadow);
-  // Composite.addBody(carComposite, carImage);
+  Composite.addBody(carComposite, carShadow);
+  Composite.addBody(carComposite, carImage);
   return carComposite;
 };
 
